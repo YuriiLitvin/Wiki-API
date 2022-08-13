712e7f863e1a9c9c0db281556279b2029c8cb3e1
Юрій <yurijlitviniii@gmail.com>
2022-08-13 18:33:04 +0300

    app.js: Create delete() in route() chain

--- app.js
+++ app.js
@@ -96,6 +96,17 @@ app.route("/articles/:articleTitle")
         res.send(err);
       }
     });
+})
+
+.delete(function(req, res)  {
+  Article.deleteOne(
+    {title: req.params.articleTitle}, function(err) {
+      if (!err) {
+        res.send("Successfully deleted article.")
+      } else {
+        res.send(err);
+      }
+    });
 });
 
 
